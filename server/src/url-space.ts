11749af8e84da372c315e7a502803a779c27a156
ark-genex <Shri@thearkinnovations.com>
2018-06-11 23:44:08 -0400

    resolving urlSpace

--- server/src/url-space.ts
+++ server/src/url-space.ts
@@ -1,37 +1,22 @@
-/*export interface IURLS {
-  local: ILocal;
-  hirely: IHirely;
-  headers: IHeaders;
-}
-
-export interface ILocal {
-  user: string;
-  userHistory: string;
-}
-
-export interface IHirely {
-  user: string;
-  userHistory: string;
-}
-
-export interface IHeaders {
-
-}*/
-
-
-export class UrlSpace /*implements IURLS*/ {
-  /*headers: IHeaders;
-  hirely: IHirely;
-  local: ILocal;*/
-
+export class UrlSpace {
 
   /* LOCAL URLS */
-  private static readonly LOCAL_USER = '/api/user';
-  private static readonly LOCAL_USER_HISTORY = '/api/users/:username/history';
+
+  static readonly LOCAL_USER = '/api/user';
+  static readonly LOCAL_USER_HISTORY = '/api/users/:username/history';
 
   /* HIRELY API URLS */
-  private static readonly HIRELY_USER = '/api/users?limit={limit}&page={page}&sorts={sorts}&search={search}';
-  private static readonly HIRELY_USER_HISTORY = '/api/users/{userName}/history?limit={limit}&page={page}&sorts={sorts}&fields={fields}&search={search}';
 
+  static readonly HIRELY_USER = '/api/users?limit={limit}&page={page}&sorts={sorts}&search={search}';
+  static readonly HIRELY_USER_HISTORY = '/api/users/{userName}/history?limit={limit}&page={page}&sorts={sorts}&fields={fields}&search={search}';
+
+  /* HEADERS */
+
+  static readonly HEADERS_HIRELY_ACCEPT_V1 = 'application/vnd.hirely-v1.0+json';
+  static readonly HEADERS_HIRELY_ACCEPT_V2 = 'application/vnd.hirely-v2.0+json';
+
+  static readonly HEADERS_CONTENT_TYPE_NAME = 'Content-Type';
+  static readonly HEADERS_CONTENT_TYPE_JSON = 'application/json';
+  static readonly HEADERS_CONTENT_TYPE_HTML = 'text/html';
 
 }
